767a22ba5bca460c09b950f1d7f1ec860f948359
carlomarcon <carlomarcon1998@gmail.com>
2023-11-14 17:48:55 +0100

    ADD. SLIDES SWITCH

--- vue-slider-markup-base/js/script.js
+++ vue-slider-markup-base/js/script.js
@@ -1,37 +1,78 @@
 const { createApp } = Vue;
 
-const slides = [
-  {
-    image: "img/01.webp",
-    title: "Marvel's Spiderman Miles Morale",
-    text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-  },
-  {
-    image: "img/02.webp",
-    title: "Ratchet & Clank: Rift Apart",
-    text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-  },
-  {
-    image: "img/03.webp",
-    title: "Fortnite",
-    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-  },
-  {
-    image: "img/04.webp",
-    title: "Stray",
-    text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-  },
-  {
-    image: "img/05.webp",
-    title: "Marvel's Avengers",
-    text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-  },
-];
+// const slides = [
+//   {
+//     image: "img/01.webp",
+//     title: "Marvel's Spiderman Miles Morale",
+//     text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+//   },
+//   {
+//     image: "img/02.webp",
+//     title: "Ratchet & Clank: Rift Apart",
+//     text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+//   },
+//   {
+//     image: "img/03.webp",
+//     title: "Fortnite",
+//     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+//   },
+//   {
+//     image: "img/04.webp",
+//     title: "Stray",
+//     text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+//   },
+//   {
+//     image: "img/05.webp",
+//     title: "Marvel's Avengers",
+//     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+//   },
+// ];
 
 createApp({
   data() {
     return {
-      text: "hello",
+      slides: [
+        {
+          image: "img/01.webp",
+          title: "Marvel's Spiderman Miles Morale",
+          text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+        },
+        {
+          image: "img/02.webp",
+          title: "Ratchet & Clank: Rift Apart",
+          text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+        },
+        {
+          image: "img/03.webp",
+          title: "Fortnite",
+          text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        },
+        {
+          image: "img/04.webp",
+          title: "Stray",
+          text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+        },
+        {
+          image: "img/05.webp",
+          title: "Marvel's Avengers",
+          text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+        },
+      ],
+      slideIndex: 0,
     };
+  },
+  methods: {
+    Change: function () {
+      this.slideIndex++;
+      if (this.slideIndex === 5) {
+        this.slideIndex = 0;
+      }
+    },
+    ChangeBack: function () {
+      this.slideIndex--;
+      if (this.slideIndex === -1) {
+        this.slideIndex = 4;
+      }
+    },
   },
 }).mount("#app");
